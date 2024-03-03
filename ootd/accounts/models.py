aaae23d99589f989e9a3f9a9bfc12b1963de1504
mummylovestocode <devikarajeevan2004@gmail.com>
2024-03-03 10:11:02 +0530

    folder extra added

--- ootd/accounts/models.py
+++
@@ -1,7 +0,0 @@
-from django.contrib.auth.models import User
-from django.db import models
-
-class UserProfile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    phone_number = models.CharField(max_length=20, blank=True)
-    shipping_address = models.TextField(blank=True)
